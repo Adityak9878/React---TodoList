c18dd38a0071bdcf2ba3b97fa1f243ebc5bf0d5c
Aditya <adityak979@gmail.com>
2024-06-13 10:43:44 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import CssBaseline from "@mui/material/CssBaseline"
+import "./App.css"
+import TodoList from "./TodoList"
+
+function App() {
+  return (
+    <>
+    <CssBaseline />
+    <h1>Todo</h1>
+    < TodoList/>
+    </>
+  )
+}
+
+export default App
